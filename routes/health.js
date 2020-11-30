fae39770c2d660758a2aec0dd4080883a241742a
Michiel Brinkman <jmbrinkman@outlook.com>
2020-11-30 16:41:08 +0100

    moo

--- routes/health.js
+++ routes/health.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
-  
+
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date());
+  next();
+})
+
 router.get('/', function (req, res, next) {
   res.json({status: 'UP'});
 });
+
+module.exports = router;
